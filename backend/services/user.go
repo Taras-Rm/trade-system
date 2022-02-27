384f3dd70487377b8823aea2ac3ed5d768a62b0e
Taras-Rm <tarasrom25@gmail.com>
2022-02-27 11:54:21 +0200

    changes in login struct

--- backend/services/user.go
+++ backend/services/user.go
@@ -21,8 +21,8 @@ type UserRegister struct {
 }
 
 type UserLogin struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 type TokensRequest struct {
