6f4fbd9a4132110299365b1c3bd9fcd3595c3035
Taras-Rm <tarasrom25@gmail.com>
2022-05-24 20:30:14 +0300

    added refresh token endpoint

--- backend/api/auth.go
+++ backend/api/auth.go
@@ -14,6 +14,7 @@ func InjectAuth(gr *gin.RouterGroup, userService services.UserService) {
 	handler.POST("register", register(userService))
 	handler.POST("login", login(userService))
 	handler.POST("loginByToken", loginByToken(userService))
+	handler.POST("refresh", refreshTokens(userService))
 	handler.POST("logout", logout(userService))
 }
 
@@ -91,6 +92,32 @@ func loginByToken(userService services.UserService) gin.HandlerFunc {
 		}
 
 		c.JSON(http.StatusOK, gin.H{"message": "success authorized", "data": res})
+	}
+}
+
+func refreshTokens(userService services.UserService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var req *services.RefreshRequest
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "bad request",
+				"error":   err.Error(),
+			})
+			return
+		}
+		res, err := userService.RefreshTokens(req)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"message": "server error",
+				"error":   err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Authenticated",
+			"data":    res,
+		})
 	}
 }
 
